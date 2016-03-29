270f14ad86af3b9974eab2641e361b0251bd0fc1
karpukdm <offspace@yandex.ru>
2016-03-29 17:55:33 +0300

    add module webapp

--- database/src/main/java/com/routefinder/repository/AccountRepository.java
+++ database/src/main/java/com/routefinder/repository/AccountRepository.java
@@ -3,14 +3,16 @@ package com.routefinder.repository;
 import com.routefinder.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * Created by karpukdm on 26.03.16.
  */
 public interface AccountRepository extends JpaRepository<Account, Integer>{
 
     Account findOneAccountById(Integer id);
-
     Account findOneAccountByLogin(String login);
 
     void deleteOneById(Integer id);
+    void deleteOneByLogin(String login);
 }
--- database/src/test/com/routefinder/repository/AccountRepositoryTest.java
+++ database/src/test/com/routefinder/repository/AccountRepositoryTest.java
@@ -1,8 +1,8 @@
 package com.routefinder.repository;
 
-import com.routefinder.BeforeMethod;
 import com.routefinder.model.Account;
 import com.routefinder.model.Role;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,13 +19,18 @@ import java.util.List;
 @DirtiesContext
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:database-config/database-context.xml")
-public class AccountRepositoryTest extends BeforeMethod{
+public class AccountRepositoryTest{
+
+    @Before
+    public void before(){
+        accountRepository.deleteAll();
+    }
 
     @Autowired
     private AccountRepository accountRepository;
 
     @Test
-    public void AddAccount(){
+    public void addAccount(){
         Account account = new Account("KarpukDM", "1111");
         List<Role> roles = new LinkedList<>();
         roles.add(new Role("ROLE_ADMIN"));
@@ -35,7 +40,7 @@ public class AccountRepositoryTest extends BeforeMethod{
     }
 
     @Test
-    public void FindByLogin(){
+    public void findByLogin(){
         Account user = accountRepository.findOneAccountByLogin("KarpukDM");
         if(user != null) {
             System.out.println(user.toString());
@@ -43,15 +48,17 @@ public class AccountRepositoryTest extends BeforeMethod{
     }
 
     @Test
-    public void FindById(){
-        Account user = accountRepository.findOneAccountById(1);
-        if(user != null) {
-            System.out.println(user.toString());
-        }
+    public void findByRole(){
+        List<Account> accounts = accountRepository.findByRole("ROLE_ADMIN");
     }
 
     @Test
-    public void DeleteById(){
+    public void deleteById(){
         accountRepository.deleteOneById(1);
+    }
+
+    @Test
+    public void deleteByLogin(){
+        accountRepository.deleteOneByLogin("KarpukDM");
     }
 }
